81e122e233b9e03fe572e6b65031c77a45cda7af
Arthur <saintgrikus@gmail.com>
2022-03-24 17:28:02 +0200

    finish return

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -4,12 +4,20 @@ public class ClassesAndObjects {
         person1.name = "Roman";
         person1.age = 50;
         person1.speak();
+        //person1.calculateYearsToRetirement();
+        int years1 = person1.calculateYearsToRetirement();
+        System.out.println("To Retirement "+years1);
         //не правильный метод System.out.println("My neame is "+person1.name+". "+"I have "+person1.age+" "+"years old");
         Person person2 = new Person();
         person2.name = "Vova";
         person2.age = 28;
         //не правильный методSystem.out.println("My neame is "+person2.name+". "+"I have "+person2.age+" "+"years old");
+        person2.speak();
+        //person2.calculateYearsToRetirement();
         person2.sayHello();
+        int years2 = person2.calculateYearsToRetirement();
+        System.out.println("To Retirement "+years2);
+
     }
 }
 
@@ -19,6 +27,13 @@ class Person{
     //2.Действия которые он может выполнять(методы)
     String name;
     int age;
+
+    int calculateYearsToRetirement(){   //годы до пенсии(перевод)
+        int years = 65-age;
+        //для вывода в чат System.out.println("Years To Retirement "+years);
+        return years;
+    }
+
     void speak(){
         //for (int i = 0; i < 3; i++) {
             System.out.println("My neame is " + name + ". " + "I have " + age + " " + "years old");
