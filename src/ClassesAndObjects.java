d4226a77725ae989dfbfd3cb4896903639dd8aa3
Arthur <saintgrikus@gmail.com>
2022-03-24 17:15:28 +0200

    finish metods

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -3,11 +3,13 @@ public class ClassesAndObjects {
         Person person1 = new Person();
         person1.name = "Roman";
         person1.age = 50;
-        System.out.println("My neame is "+person1.name+". "+"I have "+person1.age+" "+"years old");
+        person1.speak();
+        //не правильный метод System.out.println("My neame is "+person1.name+". "+"I have "+person1.age+" "+"years old");
         Person person2 = new Person();
         person2.name = "Vova";
         person2.age = 28;
-        System.out.println("My neame is "+person2.name+". "+"I have "+person2.age+" "+"years old");
+        //не правильный методSystem.out.println("My neame is "+person2.name+". "+"I have "+person2.age+" "+"years old");
+        person2.sayHello();
     }
 }
 
@@ -17,4 +19,12 @@ class Person{
     //2.Действия которые он может выполнять(методы)
     String name;
     int age;
+    void speak(){
+        //for (int i = 0; i < 3; i++) {
+            System.out.println("My neame is " + name + ". " + "I have " + age + " " + "years old");
+        //}
+    }
+    void sayHello(){
+        System.out.println("Hello!");
+    }
 }
