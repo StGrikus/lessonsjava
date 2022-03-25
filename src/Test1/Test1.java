ee623bbeeeba050bafbe7a9e7394a653c17c8b80
Arthur <saintgrikus@gmail.com>
2022-03-25 22:35:27 +0200

    change Test1

--- src/Test1/Test1.java
+++ src/Test1/Test1.java
@@ -4,26 +4,45 @@ import java.util.Scanner;
 
 public class Test1 {
     public static void main(String[] args) {
+        Person person1 = new Person();
         Scanner na = new Scanner(System.in);
         System.out.println("Введите имя");
-        String name = na.next();
+        String n = na.next();
+        person1.setName(n);
         Scanner ag = new Scanner(System.in);
         System.out.println("Введите ваш возраст");
-        int age = ag.nextInt();
-        Person person1 = new Person();
-        person1.setNameAndAge(name, age);
-        person1.speak();
+        int a = ag.nextInt();
+        person1.setAge(a);
+        System.out.println("Моё имя "+person1.getName()+". Мой возраст "+person1.getAge());
     }
 }
 class Person{
-    private String name;
-    private int age;
+    private String n;
+    private int a;
 
-    void setNameAndAge(String user_name, int user_age){
-        name = user_name;
-        age = user_age;
+    public void setName(String user_name){
+        if (user_name.isEmpty()) {
+            System.out.println("Поле с именем пустое");
+        }
+        else {
+            n = user_name;
+        }
     }
-    void speak(){
-        System.out.println("Моё имя "+name+". Мой возраст "+age);
+
+    public void setAge(int user_age) {
+        if (user_age < 0) {
+            System.out.println("Неоректный возраст");
+        }
+        else {
+            a = user_age;
+        }
+    }
+
+    public String getName(){
+        return n;
+    }
+
+    public int getAge(){
+        return a;
     }
 }
