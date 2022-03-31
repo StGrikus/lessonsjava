bbfaa664a261aaad355f27f5cadaf5e0ca27f314
Arthur <saintgrikus@gmail.com>
2022-03-31 20:14:01 +0300

    srtings function

---
+++ src/Lessons24.java
@@ -0,0 +1,7 @@
+public class Lessons24 { //String lessons
+    public static void main(String[] args) {
+        String x = "Hello";
+        x = x.toUpperCase();
+        System.out.println(x);
+    }
+}
