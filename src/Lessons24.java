6d0ea880b984e491cfa97b5f713f71398710b219
Arthur <saintgrikus@gmail.com>
2022-03-31 20:39:56 +0300

    finish System.out.print/System.out.printlf

--- src/Lessons24.java
+++ src/Lessons24.java
@@ -1,7 +1,17 @@
-public class Lessons24 { //String lessons
+public class Lessons24 {
+    //LESSONS24
+    //public static void main(String[] args) {
+    //    StringBuilder sb = new StringBuilder("hello");
+    //    sb.append(" my").append(" friend");
+    //    System.out.println(sb.toString());
+    //}
+    //LESSONS25
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("hello");
-        sb.append(" my").append(" friend");
-        System.out.println(sb.toString());
+        System.out.printf("this is a string %.2f \n", 15.3511);
+        System.out.printf("this is a string %.2f \n", 24.154);
+        System.out.printf("this is a string %.2f \n", 99.8595);
+        System.out.printf("this is a string %.2f \n", 11.698999);
+        // % [10] - ширина числа
+        // % .[2] - количество символов после запятой
     }
 }
