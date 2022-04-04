18762507d3cd5edfadc21a75a7f82dfa74d5fd88
Arthur <saintgrikus@gmail.com>
2022-04-04 22:12:19 +0300

    start Interfaces

--- src/Interfaces/Animal.java
+++ src/Interfaces/Animal.java
@@ -3,4 +3,11 @@ package Interfaces;
 public class Animal {
     public int id;
 
+    public Animal(int id){
+        this.id = id;
+    }
+
+    public void sleep(){
+        System.out.println("i`m sleeping");
+    }
 }
